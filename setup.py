31bbf06e02fd87793ce804b2bc50dbcd8aff73ab
Dmitriy F <schahmatist@hotmail.com>
2022-04-18 13:47:41 -0400

    Preparing first push

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='chicago_crush_analysis',
+      version='POC',
+      url='https://github.com/Dima and Austin/chicago_crush_analysis.git',
+      author='Dima and Austin',
+      packages=['src'],
+      zip_safe=False)
